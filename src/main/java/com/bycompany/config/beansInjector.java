6fe0a88f2d946c8f11eb758eaa2d02e4af7cfec3
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-06-23 02:11:45 -0500

    Logro: aprendi a eliminar ambiguedades con @Primary y @Qualifyer

--- src/main/java/com/bycompany/config/beansInjector.java
+++ src/main/java/com/bycompany/config/beansInjector.java
@@ -6,20 +6,23 @@ import com.bycompany.repository.ProductRepositoryB;
 import com.bycompany.service.ProductService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class beansInjector {
 
     public beansInjector() {
-        System.out.println("creando instancia de "+ this.getClass().getSimpleName());
+        System.out.println("creando instancia de " + this.getClass().getSimpleName());
     }
 
-    @Bean
+    @Bean("productRepositoryXML")
     public ProductRepositoryA createProductRepositoryA(){
         return new ProductRepositoryA();
     }
 
-    @Bean
+    @Bean("productRepositoryMySQL")
+    //@Primary
     public ProductRepositoryB createproductRepositoryB(){
         return new ProductRepositoryB();
     }
--- src/main/java/com/bycompany/service/ProductService.java
+++ src/main/java/com/bycompany/service/ProductService.java
@@ -1,20 +1,21 @@
 package com.bycompany.service;
 
 import com.bycompany.repository.ProductRepository;
-import com.bycompany.repository.ProductRepositoryA;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class ProductService {
+    @Qualifier("productRepositoryMySQL")
     @Autowired
-    private ProductRepositoryA productRepositoryA;
+    private ProductRepository productRepository;
 
     public ProductService() {
         System.out.println("creando instancia de "+ this.getClass().getSimpleName());
     }
     public void save(String name){
-        productRepositoryA.save(name);
+        productRepository.save(name);
     }
     public void remove(String name){
-        productRepositoryA.remove(name);
+        productRepository.remove(name);
     }
 }
